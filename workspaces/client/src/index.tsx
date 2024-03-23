7fc9920c0577943228dfbec47f1a31f7a03e2665
kyken <dyfyb9@gmail.com>
2024-03-24 01:22:07 +0900

    update: admin

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -5,10 +5,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SWRConfig } from 'swr';
 
-// import { AdminApp } from '@wsh-2024/admin/src/index';
+import { AdminApp } from '@wsh-2024/admin/src/index';
 import { ClientApp } from '@wsh-2024/app/src/index';
 
-import { preloadImages } from './utils/preloadImages';
+// import { preloadImages } from './utils/preloadImages';
 import { registerServiceWorker } from './utils/registerServiceWorker';
 
 const main = async () => {
@@ -17,7 +17,7 @@ const main = async () => {
 
   $(document).ready(() => {
     if (window.location.pathname.startsWith('/admin')) {
-      // ReactDOM.createRoot($('#root').get(0)!).render(<AdminApp />);
+      ReactDOM.createRoot($('#root').get(0)!).render(<AdminApp />);
     } else {
       ReactDOM.hydrateRoot(
         $('#root').get(0)!,
