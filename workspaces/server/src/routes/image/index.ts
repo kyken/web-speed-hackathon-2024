7f220658a478fcce54bf84bd529846ce0b71aa44
kyken <dyfyb9@gmail.com>
2024-03-24 01:46:15 +0900

    commit

--- workspaces/server/src/routes/image/index.ts
+++ workspaces/server/src/routes/image/index.ts
@@ -5,6 +5,7 @@ import path from 'node:path';
 
 import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
+import { compress } from 'hono/compress';
 import { HTTPException } from 'hono/http-exception';
 import { Image } from 'image-js';
 import { z } from 'zod';
@@ -62,6 +63,8 @@ const IMAGE_CONVERTER: Record<SupportedImageExtension, ConverterInterface> = {
 };
 
 const app = new Hono();
+
+app.use(compress());
 
 app.get(
   '/images/:imageFile',
